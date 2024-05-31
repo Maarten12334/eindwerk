0997064c1bc8ca445d0deed1365b4d86f604e68c
Maarten <maarten.wils1234@gmail.com>
2024-05-31 10:20:36 +0200

    Added Search flight button when no flight number is given

--- app/Livewire/AddItemsToItinerary.php
+++ app/Livewire/AddItemsToItinerary.php
@@ -4,20 +4,17 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\ItineraryItem;
-use Carbon\Carbon;
 
 class AddItemsToItinerary extends Component
 {
     public $current_date;
-    public $message = 'Add Items';
-    public $buttonClicked = false;
-    public $newItem = '';
     public $itinerary_id;
+    public $newItem;
+    public $buttonClicked = false;
+    public $editItem;
+    public $message = 'Add Item';
 
-    public function render()
-    {
-        return view('livewire.add-items-to-itinerary');
-    }
+    protected $listeners = ['editItem', 'deleteItem'];
 
     public function addItem()
     {
@@ -26,24 +23,63 @@ class AddItemsToItinerary extends Component
 
     public function saveItem()
     {
-        // Validate the input
         $this->validate([
             'newItem' => 'required|string|max:255',
         ]);
 
-        $formattedDate = Carbon::parse($this->current_date)->format('Y-m-d');
-
-        // Save the item to the itinerary items table
         ItineraryItem::create([
-            'date' => $formattedDate,
+            'itinerary_id' => $this->itinerary_id,
+            'date' => $this->current_date,
             'type' => $this->newItem,
-            'itinerary_id' => $this->itinerary_id
+            'description' => '',
         ]);
 
-        // Reset the input field and button state
+        $this->resetInput();
+        $this->emit('refreshComponent');
+    }
+
+    public function editItem($itemId)
+    {
+        $item = ItineraryItem::findOrFail($itemId);
+        $this->editItem = $item;
+        $this->newItem = $item->type;
+        $this->buttonClicked = true;
+        $this->message = 'Edit Item';
+    }
+
+    public function updateItem()
+    {
+        $this->validate([
+            'newItem' => 'required|string|max:255',
+        ]);
+
+        if ($this->editItem) {
+            $item = ItineraryItem::findOrFail($this->editItem->id);
+            $item->update([
+                'type' => $this->newItem,
+            ]);
+            $this->resetInput();
+            $this->emit('refreshComponent');
+        }
+    }
+
+    public function deleteItem($itemId)
+    {
+        $item = ItineraryItem::findOrFail($itemId);
+        $item->delete();
+        $this->emit('refreshComponent');
+    }
+
+    private function resetInput()
+    {
         $this->newItem = '';
         $this->buttonClicked = false;
+        $this->editItem = null;
+        $this->message = 'Add Item';
+    }
 
-        // You can add additional steps here if needed (e.g., refresh the items list)
+    public function render()
+    {
+        return view('livewire.add-items-to-itinerary');
     }
 }
--- resources/views/itineraries/show.blade.php
+++ resources/views/itineraries/show.blade.php
@@ -84,6 +84,7 @@
                 <div class="mt-6">
                     <a href="{{ route('itineraries.index') }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My itineraries</a>
                     <a href="{{ route('itineraries.edit', $itinerary->id) }}" class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-4 rounded">Edit itinerary</a>
+                    @livewire('SearchFlightButton', ['itinerary' => $itinerary])
                 </div>
             </div>
         </div>
