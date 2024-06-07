92b520c1c5cb8881d50a07de310ccdf0524f407d
Maarten <maarten.wils1234@gmail.com>
2024-06-07 12:39:09 +0200

    Kleine aanpassingen

--- resources/views/hotels/results.blade.php
+++ resources/views/hotels/results.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-primaryGreen leading-tight">
-            Vluchten
+            Hotels
         </h2>
     </x-slot>
 
