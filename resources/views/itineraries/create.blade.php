1e545e3d5f372a5d63f2ddb46c2effd83a55fd9b
Maarten <maarten.wils1234@gmail.com>
2024-06-16 11:53:33 +0200

    Updated create itinerary.blade

--- resources/views/itineraries/create.blade.php
+++ resources/views/itineraries/create.blade.php
@@ -1,42 +1,40 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <div class="bg-primaryGreen py-6 shadow">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h2 class="font-semibold text-xl text-secondaryGreen leading-tight">
+                    {{ __('Maak een nieuwe reis') }}
+                </h2>
+            </div>
+        </div>
+    </x-slot>
 
-<head>
-    <title>Create Itinerary</title>
-</head>
-
-@section('header')
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    {{ __('Search Hotels by City') }}
-</h2>
-@endsection
-
-@section('content')
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <form action="{{ route('itineraries.store') }}" method="POST">
-                    @csrf
-                    <div class="form-group mb-6">
-                        <label for="itineraryName" class="block mb-2 dark:text-white">Itinerary Name</label>
-                        <input type="text" name="name" id="itineraryName" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
-                    </div>
-                    <div class="form-group mb-6">
-                        <label for="departureDate" class="block mb-2 dark:text-white">Departure date</label>
-                        <input type="date" name="departure" id="departureDate" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
-                    </div>
-                    <div class="form-group mb-6">
-                        <label for="returnDate" class="block mb-2 dark:text-white">Return Date</label>
-                        <input type="date" name="return" id="returnDate" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
-                    </div>
-                    <div class="form-group mb-6">
-                        <label for="flightId" class="block mb-2 dark:text-white">Flight id (optional)</label>
-                        <input type="text" name="flight_id" id="flightId" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded">
-                    </div>
-                    <button type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Create Itinerary</button>
-                </form>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('itineraries.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group mb-6">
+                            <label for="itineraryName" class="block mb-2 dark:text-white">Naam reis</label>
+                            <input type="text" name="name" id="itineraryName" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
+                        </div>
+                        <div class="form-group mb-6">
+                            <label for="departureDate" class="block mb-2 dark:text-white">Vertrek datum</label>
+                            <input type="date" name="departure" id="departureDate" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
+                        </div>
+                        <div class="form-group mb-6">
+                            <label for="returnDate" class="block mb-2 dark:text-white">Eind datum</label>
+                            <input type="date" name="return" id="returnDate" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded" required>
+                        </div>
+                        <div class="form-group mb-6">
+                            <label for="flightId" class="block mb-2 dark:text-white">Vlucht Id (optioneel)</label>
+                            <input type="text" name="flight_id" id="flightId" class="form-control w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded">
+                        </div>
+                        <button type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Opslaan</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</x-app-layout>
